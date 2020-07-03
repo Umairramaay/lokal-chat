daffcb34bfff189183afdf55bcdd68c8aa319db2
Umairramay <umairshabr@gmail.com>
2020-07-04 00:25:27 +0500

    Add all my files

---
+++ src/settings.js
@@ -0,0 +1,14 @@
+// let DEBUG = false;
+// let HOST_URL = 'https://lokal-chat.herokuapp.com'
+// let SOCKET_URL = "wss://lokal-chat.herokuapp.com";
+// if (DEBUG) {
+//   HOST_URL = "http://127.0.0.1:8000";
+//   SOCKET_URL = "ws://127.0.0.1:8000";
+// }
+
+let HOST_URL = "http://127.0.0.1:8000";
+let SOCKET_URL = "ws://127.0.0.1:8000";
+
+
+
+export { HOST_URL, SOCKET_URL };
